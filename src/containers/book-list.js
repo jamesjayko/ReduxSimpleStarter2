6f9b33286889db6cd08ba003071b2b96933dcca7
James Ko <james.ko@James-Kos-iMac.local>
2018-08-14 15:43:42 -0700

    added bookList to state to props and listed title on page

---
+++ src/containers/book-list.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+
+class BookList extends Component {
+	constructor(props) {
+        super(props);
+
+        this.state
+	}
+
+	renderList() {
+		return this.props.books.map(book => {
+			return (
+				<li key={book.title} className="list-group-item">
+					{book.title}
+				</li>
+			);
+		});
+	}
+
+	render() {
+		return <ul className="list-group col-sm-4">{this.renderList()}</ul>;
+	}
+}
+
+function mapStateToProps(state) {
+	// whatever is returned will show up as this.props inside of BookList
+	return{
+		books: state.books
+	}
+	
+}
+
+export default connect(mapStateToProps)(BookList);
---
+++ src/reducers/reducer_books.js
@@ -0,0 +1,9 @@
+export default function() {
+    return [
+        {title: 'Harry Potter'},
+        {title: 'JavaScript: The Good Parts'},
+        {title: 'The Bible'},
+        {title: 'S.A.L.T'},
+        {title: 'The Dark Tower'},
+    ]
+}
