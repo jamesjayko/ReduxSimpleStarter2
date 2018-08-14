4a58384f4abfe6e12c3600a5c30e4673df8f48d2
James Ko <james.ko@James-Kos-iMac.local>
2018-08-14 16:46:26 -0700

    added book detail reducer and action

---
+++ src/containers/book-detail.js
@@ -0,0 +1,19 @@
+import React, { Component } from 'react';
+import { connect } from "react-redux";
+
+class BookDetail extends Component {
+    render(){
+        return (
+            <div>Book Detail</div>
+        )
+    }
+}
+
+function mapStateToProps(state) {
+    console.log(state);
+    return {
+        book: state.activeBook
+    }
+}
+
+export default connect(mapStateToProps)(BookDetail);
