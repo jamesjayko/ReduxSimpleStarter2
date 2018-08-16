3a621c907ab0176fa335631141a8d6c59f9e1ffd
James Ko <james.ko@James-Kos-iMac.local>
2018-08-16 14:49:33 -0700

    successful axios call

--- src/containers/search_bar.js
+++ src/containers/search_bar.js
@@ -1,32 +1,41 @@
 import React, { Component } from 'react';
 import { format } from 'util';
 
+import { connect } from 'react-redux';
+import { bindActionCreators } from 'redux';
+import { fetchWeather } from '../actions/index';
+
 class SearchBar extends Component {
 	constructor(props) {
 		super(props);
 
 		this.state = {
 			term: ''
-        };
-        
-        // bind the function to 'this' or you can use fat arrow on onInputChange to always point to component's 'this'
-		this.onInputChange = this.onInputChange.bind(this); 
+		};
+
+		// bind the function to 'this' or you can use fat arrow on onInputChange to always point to component's 'this'
+		this.onInputChange = this.onInputChange.bind(this);
+		this.onFormSubmit = this.onFormSubmit.bind(this);
 	}
 
 	onInputChange(event) {
 		this.setState({
 			term: event.target.value
 		});
-    }
-    
+	}
+
 	onFormSubmit(event) {
-        // preventdefault will prevent it from "submit" hence won't refresh the page
+		// preventdefault will prevent it from "submit" hence won't refresh the page
 		event.preventDefault();
+
+		// fetch weather data
+		this.props.fetchWeather(this.state.term);
+		this.setState({ term: '' });
 	}
 
 	render() {
 		return (
-            // html form element will try to submit when pressed 'enter' in any children elements as well.
+			// html form element will try to submit when pressed 'enter' in any children elements as well.
 			<form className="input-group" onSubmit={this.onFormSubmit}>
 				<input
 					type="text"
@@ -45,4 +54,11 @@ class SearchBar extends Component {
 	}
 }
 
-export default SearchBar;
+function mapDispatchToProps(dispatch) {
+	return bindActionCreators({ fetchWeather }, dispatch);
+}
+
+export default connect(
+	null,
+	mapDispatchToProps
+)(SearchBar);
