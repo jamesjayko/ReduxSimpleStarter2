1299e64218b05d6f1424ac26aafd913670ccdaf2
James Ko <james.ko@James-Kos-iMac.local>
2018-08-16 15:10:15 -0700

    implemented redux-promise middleware for our axios payload

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,17 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 
 const API_KEY = 'a5faad6a3b4ba524f1ea27ed2102be0c';
 const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
-export function fetchWeather (city) {
+export function fetchWeather(city) {
+	const url = `${ROOT_URL}&q=${city},us`;
+    // redux-promise middleware will interupt this axios call (which will return a promise by nature) 
+    // and make it regular data to send to all the reducers instead of a promise object.
+	const request = axios.get(url);
 
-    const url = `${ROOT_URL}&q=${city},us`;
-    const request = axios.get(url);
+	// console.log('request:', request); // proof that it returns a promise not data.
 
-    return {
-        type: FETCH_WEATHER,
-        payload: request
-    };
+	return {
+		type: FETCH_WEATHER,
+		payload: request
+	};
 }
