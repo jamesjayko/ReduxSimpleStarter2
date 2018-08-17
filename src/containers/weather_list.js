a8523cae27b5044b7a125d9ff5fa00aac769b175
James Ko <james.ko@James-Kos-iMac.local>
2018-08-17 12:04:46 -0700

    added react-sparklines chart and passed correct data to populate it

--- src/containers/weather_list.js
+++ src/containers/weather_list.js
@@ -1,33 +1,49 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import Chart from '../components/chart'
 
 class WeatherList extends Component {
-    render() {
-        return (
-            <table className= 'table table-hover'>
-            <thead>
-                <tr>
-                    <th>
-                        City
-                    </th>
-                    <th>
-                        Temperature
-                    </th>
-                    <th>
-                        Pressure
-                    </th>
-                    <th>
-                        Humidity
-                    </th>
-                </tr>
-            </thead>
-            </table>
-        )
-    }
+	renderWeather(cityData) {
+		const cityName = cityData.city.name;
+		const temps = cityData.list.map(weather => weather.main.temp);
+		const pressures = cityData.list.map(weather => weather.main.pressure);
+		const humidities = cityData.list.map(weather => weather.main.humidity);
+
+		return (
+			<tr key={cityName}>
+				<td>{cityName}</td>
+				<td>
+                    <Chart data={temps} color='orange' />
+                </td>
+				<td>
+                    <Chart data={pressures} color='green' />
+                </td>
+				<td>
+                    <Chart data={humidities} color='black' />
+                </td>
+			</tr>
+		);
+	}
+
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>City</th>
+						<th>Temperature</th>
+						<th>Pressure</th>
+						<th>Humidity</th>
+					</tr>
+				</thead>
+				<tbody>{this.props.weather.map(this.renderWeather)}</tbody>
+			</table>
+		);
+	}
 }
 
 function mapStateToProps(state) {
-    return { weather: state.weather }
+	return { weather: state.weather };
 }
 
 export default connect(mapStateToProps)(WeatherList);
